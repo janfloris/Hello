87734893bbfa76bbe4d8503717887d0044fd7428
Jan Floris <janfloris@gmail.com>
2024-01-11 15:04:29 -0600

    **

--- kwargs.py
+++ kwargs.py
@@ -1,7 +1,7 @@
-def example_function(it):
-    for key, value in it.items():
+def example_function(**kwargs):
+    for key, value in kwargs.items():
         print(f"{key}: {value}")
 
 # Using double asterisks to unpack a dictionary
 my_dict = {'name': 'John', 'age': 25, 'city': 'New York'}
-example_function(my_dict)
+example_function(**my_dict)
